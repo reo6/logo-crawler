5e05d4030c628d44cfb6f1eee4163d7d769cbd9a
reo006 <ramazanemreosmanoglu@gmail.com>
2024-08-02 15:41:33 +0300

    Migrate from Python to Rust

--- default.nix
+++ default.nix
@@ -1,24 +1,20 @@
 let
-    pkgs = (import (builtins.fetchTarball {
-        url = "https://github.com/NixOS/nixpkgs/archive/63dacb46bf939521bdc93981b4cbb7ecb58427a0.zip";
-        sha256 = "1lr1h35prqkd1mkmzriwlpvxcb34kmhc9dnr48gkm8hh089hifmx";
-    }) { });
-    stdenv = pkgs.stdenv;
+  pkgs = (import (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/63dacb46bf939521bdc93981b4cbb7ecb58427a0.zip";
+    sha256 = "1lr1h35prqkd1mkmzriwlpvxcb34kmhc9dnr48gkm8hh089hifmx";
+  }) { });
+  stdenv = pkgs.stdenv;
 in pkgs.mkShell rec {
-    name = "interview";
-    shellHook = ''
-        source .bashrc
-    '';
-    buildInputs = (with pkgs; [
-        bashInteractive
-        (pkgs.python3.buildEnv.override {
-            ignoreCollisions = true;
-            extraLibs = with pkgs.python3.pkgs; [
-                # package list: https://search.nixos.org/packages
-                # be parsimonious with 3rd party dependencies; better to show off your own code than someone else's
-                ipython
-                nose
-            ];
-        })
-    ]);
+  name = "interview";
+  shellHook = ''
+    source .bashrc
+  '';
+  buildInputs = (with pkgs; [
+    bashInteractive
+    rustc
+    cargo
+    rustfmt
+    clippy
+  ]);
 }
+
